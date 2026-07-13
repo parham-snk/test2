2f98197e0184290b021736209d4ca5d23b23c5b1
parham-snk <psanjanaki@gmail.com>
2026-07-13 19:17:00 +0330

    node page and add-modal for nodepage

---
+++ src/router/routes/node/modals/add-node-modal.tsx
@@ -0,0 +1,135 @@
+ import { Node } from "@xyflow/react"
+import { FC, useState } from "react"
+import { useFormState } from "react-dom"
+import supapabase from "../../../../supabase"
+import { notify } from "../../../utilities/utilities"
+
+
+type modal = {
+
+    notify?: (msg: string, type: "error" | "success") => void,
+    data?: Node | null
+
+}
+
+const Add_Node_Page_Modal:FC<modal>=({data})=>{
+
+
+
+
+
+
+
+
+
+
+    let actionType = data ? true : false
+    console.log(actionType)
+    const formAction = async (p: any, formData: FormData) => {
+        const id = formData.get("id")
+        const label = formData.get("label")
+        const posx = Number(formData.get("posx")).toFixed()
+        const posy = Number(formData.get("posy")).toFixed(0)
+
+        async function InsertRow() {
+            const { error } = await supapabase.from("nodes").insert({ id, label, posx, posy })
+            if (error) {
+                return notify("error", "error")
+            } else {
+                notify("node added!", "success")
+            }
+        }
+
+        async function updateRow() {
+            const { error } = await supapabase.from("nodes").update({ id, label, posx, posy }).eq("id", id)
+            if (error) return notify("error", "error");
+            notify("node updated!", "success")
+        }
+
+        actionType ? updateRow() : InsertRow()
+
+    }
+    const [state, formaction, isPending] = useFormState(formAction, null)
+
+    const [node, setnode] = useState<any>(data ? data : null)
+    const [id, setID] = useState(node?.id || "")
+    const [label, setLabel] = useState(node?.data.label || "")
+    const [posx, setPosX] = useState(node?.position.x || "")
+    const [posy, setPosY] = useState(node?.position.y || "")
+
+
+
+    async function deleteNode() {
+        const {error}=await supapabase.rpc("delete_node",{node_id:node?.id})
+        if(error) return notify("error","error");
+        notify("node deleted!","success")
+    }
+    return (
+        <div className="bg-zinc-900 bg-opacity-70 backdrop-blur rounded shadow-xl absolute w-80 h-auto  left-16 top-10 z-20">
+            {
+                isPending &&
+                <div className="w-full h-full bg-zinc-900 bg-opacity-80 backdrop-blur  z-50 absolute
+                flex justify-center items-center align-middle
+                ">
+                    <p className="text-white text-xl">please wait!</p>
+                </div>
+            }
+            <form action={formaction} className="flex flex-col justify-start align-baseline p-3 text-white relative h-full">
+                {
+                    //id
+                }
+                <label htmlFor="id" className="py-2">id : </label>
+                <input type="text" name="id" id="id" className="rounded  p-1  bg-transparent border border-white " value={id} onChange={(e) => {
+
+                    setID(e.target.value)
+                }} />
+                {
+                    //label
+                }
+                <label htmlFor="label" className="py-2">label : </label>
+                <textarea dir={/\a-zA-Z/.test(label[0])?"ltr":"rtl"}  name="label" rows={10} id="label" className="rounded min-h-32 h-auto  p-2 bg-transparent border border-white " value={label}
+                    onChange={e => {
+                        setLabel(e.target.value)
+                    }}
+                />
+                <label htmlFor="" className="mt-4">position</label>
+                <div className="flex flex-row justify-between align-middle">
+                    <div className="flex flex-row justify-start align-middle items-center w-1/2 pe-2 mt-2">
+                        <label htmlFor="posx"> x</label>
+                        <input name="posx" id="posx" type="number" className="w-1/2 rounded  mx-2 p-1 bg-transparent border border-white text-xs"
+                            value={posx}
+                            onChange={e => {
+                                setPosX(e.target.value)
+                            }}
+                        />
+                    </div>
+                    <div className="flex flex-row justify-start align-middle items-center w-1/2 pe-2 mt-2">
+
+                        <label htmlFor="posy"> y</label>
+                        <input name="posy" id="posy" type="number" className="w-1/2 rounded  mx-2 p-1 bg-transparent border border-white text-xs"
+                            value={posy}
+                            onChange={e => {
+                                setPosY(e.target.value)
+                            }}
+                        />
+                    </div>
+                </div>
+                {
+                    actionType &&
+                    <> <input onClick={deleteNode} type="button" className="text-red-500 border border-red-500 rounded hover:bg-red-500 hover:text-black transition-all cursor-pointer
+                mt-16 p-1
+                " value={"delete node"} />
+                        <button type="submit" className="bg-white text-black rounded bottom-0 p-1 mt-2">create</button></>
+                }
+                {
+                    !actionType &&
+                    <button type="submit" className="bg-white text-black rounded bottom-0 p-1 mt-24">create</button>
+                }
+            </form>
+        </div>
+    )
+
+
+}
+
+export default Add_Node_Page_Modal
--- src/router/routes/node/node.tsx
+++ src/router/routes/node/node.tsx
@@ -1,11 +1,13 @@
 import { FC, useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import { Controls, Background, ReactFlow, applyNodeChanges ,Panel} from "@xyflow/react"
+import { Link, Navigate, useNavigate, useParams } from "react-router-dom";
+import { Controls, Background, ReactFlow, applyNodeChanges, Panel, Node } from "@xyflow/react"
 import "@xyflow/react/dist/style.css";
 import supapabase from "../../../supabase";
 import { notify } from "../../utilities/utilities";
 
-import { AiOutlineLoading } from "react-icons/ai";
+import { AiOutlineAppstoreAdd, AiOutlineLoading } from "react-icons/ai";
+import { IoMdArrowBack, IoMdExit } from "react-icons/io";
+import Add_Node_Page_Modal from "./modals/add-node-modal";
 
 
 
@@ -16,15 +18,20 @@ type nodeType = {
     data: { label: string },
 }
 
-export default function Node() {
-    
+export default function Node_Page() {
+
+    const navigate = useNavigate()
     const { node_id } = useParams<{ node_id: string }>()
     const [isloading, setIsLoading] = useState<boolean>(true)
     const [nodes, setNodes] = useState<[] | null>([])
 
+    const [modalData,setModalData]=useState<Node|null>(null)
+    const[showNodeModal,setShowNodeModal]=useState<boolean>(false)
 
     async function get_nodes() {
-        const { data, error } = await supapabase.schema("articles").from("nodes").select("*").eq("parrent_id",node_id)
+
+
+        const { data, error } = await supapabase.schema("articles").from("nodes").select("*").eq("parrent_id", node_id)
 
         setIsLoading(false)
         if (error) return notify("fetch list failed!", "error");
@@ -32,7 +39,7 @@ export default function Node() {
         data as []
         let rows = data.map(item => {
             return {
-                id: String(item.id), data: { label: item.text }, position: { x: 100, y: 100 }, drageable: true
+                id: String(item.id), data: { label: item.text }, position: { x: item.posx, y: item.posy }, drageable: true, type: "custom"
             }
         })
         setNodes(rows as any)
@@ -48,11 +55,13 @@ export default function Node() {
     }, [nodes])
 
 
+
     const MyNode: FC<{ data: { label: string } }> = ({ data }) => {
-        console.log(data)
-        return <div className="max-w-96 min-h-24 bg-white text-black">{
-            data.label
-        }</div>
+        let direction = /\a-zA-z/.test(data.label[0])
+        return <div className={"max-w-52 rounded min-h-24 bg-zinc-900 bg-opacity-30 backdrop-blur-md text-gray-300 text-sm p-2 border border-gray-500 "}>
+            <p dir={direction ? "ltr" : "rtl"} className={direction ? "text-left" : "text-right"}>{data.label}</p>
+        </div>
+
     }
     return (
         <div className="bg-zinc-900 w-screen h-screen">
@@ -72,12 +81,37 @@ export default function Node() {
                     onNodesChange={(changes) => {
                         setNodes((nds) => applyNodeChanges(changes, nds as any) as any);
                     }}
+                    onNodeClick={(eve,node)=>{
+                        setModalData(node)
+                        setShowNodeModal(true)
+                    }}
+                    nodeTypes={{ custom: MyNode }}
                     fitView
                 >
                     <Background />
                     <Controls />
                     <Panel position="top-left">
-                        <Link to={"/"}></Link>
+                        <div className="fixed p-auto left-3 top-4 bg-white text-black   z-50 flex flex-col justify-center align-baseline p-1 gap-2">
+                            <div className="cursor-pointer transition-all relative" onClick={async () => {
+                                navigate(-1)
+                            }}>
+                                <div className=" hover:scale-110 ">
+                                    <IoMdArrowBack size={20} />
+                                </div>
+                            </div>
+                            {
+                                //add-node
+                            }
+                            <div className="cursor-pointer" onClick={() => {
+
+                            }}>
+                                <AiOutlineAppstoreAdd size={20} />
+                            </div>
+                            {
+                                showNodeModal &&
+                                <Add_Node_Page_Modal data={modalData} notify={()=>{}}/>
+                            }
+                        </div>
                     </Panel>
                 </ReactFlow>
             }
